c026ae4157f275ea1364f846f3a4bfdee7eea059
Ayoyinx <ayoyinkaomotoso@gmail.com>
2023-04-12 15:33:41 -0400

    Modified message in error page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -9,14 +9,10 @@ export default function ErrorPage() {
         <div className="container mx-auto grid place-items-center h-full px-4 sm:px-0">
           <h2 className="font-serif text-6xl text-center sm:text-7xl dark:text-zinc-100 text-white">Page not found</h2>
           <p className="text-lg sm:text-xl text-center dark:text-zinc-100 text-white mt-4">
-            Unfortunately, the page you are looking for does not exist.
+            404 page does not exist.
           </p>
-          <p className="text-lg sm:text-xl text-center text-white mt-1">
+          <p className="text-lg mt-4 sm:text-xl text-center text-white mt-1">
             Create a wallet account today and fund your wallet.
-          </p>
-
-          <p>
-            <i>{error.statusText || error.message}</i>
           </p>
         </div>
       </section>
